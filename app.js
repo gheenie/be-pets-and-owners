dc33f2332596b7bdc857586e0347a16472b9c9f8
infoomics <sbx_94@hotmail.com>
2023-02-07 15:01:20 +0000

    refactored forEach() to map()

--- app.js
+++ app.js
@@ -18,18 +18,14 @@ app.get('/api/owners/:id', (req, res) => {
 app.get('/api/owners', (req, res) => {
     fs.readdir(`${__dirname}/data/owners`)
     .then((filesOfOwners) => {
-        const promisesToReadOwners = [];
-
-        filesOfOwners.forEach((fileOfOwner) => {
-            promisesToReadOwners.push( fs.readFile(`${__dirname}/data/owners/${fileOfOwner}`) );
+        const promisesToReadOwners = filesOfOwners.map((fileOfOwner) => {
+            return fs.readFile(`${__dirname}/data/owners/${fileOfOwner}`);
         });
         
         return Promise.all(promisesToReadOwners);
     })
     .then((resultsOfReadingFiles) => {
-        const owners = [];
-
-        resultsOfReadingFiles.forEach((owner) => owners.push( JSON.parse(owner) ));
+        const owners = resultsOfReadingFiles.map((owner) => JSON.parse(owner));
         
         res.status(200).send({ owners });
     });
@@ -39,18 +35,14 @@ app.get('/api/owners/:id/pets', (req, res) =>{
     const { id } = req.params
     fs.readdir(`${__dirname}/data/pets`)
     .then((filesOfPets) => {
-        const promisesToReadPets = [];
-
-        filesOfPets.forEach((fileOfPets) => {
-            promisesToReadPets.push( fs.readFile(`${__dirname}/data/Pets/${fileOfPets}`) );
+        const promisesToReadPets = filesOfPets.map((fileOfPets) => {
+            return fs.readFile(`${__dirname}/data/pets/${fileOfPets}`);
         });
         
         return Promise.all(promisesToReadPets);
     })
     .then((resultsOfReadingFiles) => {
-        const pets = [];
-
-        resultsOfReadingFiles.forEach((pet) => pets.push( JSON.parse(pet) ));
+        const pets = resultsOfReadingFiles.map((pet) => JSON.parse(pet));
 
         const ownerPet = pets.filter(pet =>{
             return pet.owner === id
@@ -65,19 +57,16 @@ app.get('/api/pets', (req, res) =>{
     const { temperament } = req.query;
     fs.readdir(`${__dirname}/data/pets`)
     .then((filesOfPets) => {
-        const promisesToReadPets = [];
-
-        filesOfPets.forEach((fileOfPets) => {
-            promisesToReadPets.push( fs.readFile(`${__dirname}/data/Pets/${fileOfPets}`) );
+        const promisesToReadPets = filesOfPets.map((fileOfPets) => {
+            return fs.readFile(`${__dirname}/data/pets/${fileOfPets}`);
         });
         
         return Promise.all(promisesToReadPets);
     })
     .then((resultsOfReadingFiles) => {
-        const pets = [];
+        const pets = resultsOfReadingFiles.map((pet) => JSON.parse(pet));
 
-        resultsOfReadingFiles.forEach((pet) => pets.push( JSON.parse(pet) ));
-
+        // if else no parameters?
         const queryPet = pets.filter(pet =>{
             return pet.temperament === temperament
         })
